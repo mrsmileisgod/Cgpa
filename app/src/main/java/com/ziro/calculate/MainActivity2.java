b0643276617d34d1a1a801cb42728f1c80d27ad7
ZirO <mrsmileisgod@gmail.com>
2021-06-07 16:10:45 +0530

    Fixed crash when no values were given

--- app/src/main/java/com/ziro/calculate/MainActivity2.java
+++ app/src/main/java/com/ziro/calculate/MainActivity2.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
     TextView textView7;
@@ -20,35 +22,48 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-
+//        Editable marks1=editText.getText();
+//        Editable marks2=editText2.getText();
         textView7=findViewById(R.id.textView7);
         textView8=findViewById(R.id.textView8);
         editText=findViewById(R.id.editText);
         editText2=findViewById(R.id.editText2);
         button3=findViewById(R.id.button3);
 
+        button3.setOnClickListener(this::getPercent);
+
     }
     @SuppressLint({"DefaultLocale", "SetTextI18n"})
+
+
     public void getPercent(View view){
-        String s1=editText.getText().toString();
-        double marks=Integer.parseInt(s1);
 
-        String s2=editText2.getText().toString();
-        double totalMarks=Integer.parseInt(s2);
-        double per=(marks/totalMarks)*100.0;
-        double cgpa=per/9.5;
-        if(per>95 && per<100){
-            textView8.setText("Your CGPA is : 10");
-            textView7.setText("Your percentage is : " + String.format("% .2f", per) + "%");
-        }
-        else if(per>100){
-            textView7.setText("Enter Valid Values!");
-            textView8.setText("");
+        try{
+            String s1 =editText.getText().toString();
+            double marks=Integer.parseInt(s1);
+            String s2=editText2.getText().toString();
+            double totalMarks=Integer.parseInt(s2);
+            double per=(marks/totalMarks)*100.0;
+            double cgpa=per/9.5;
+            if(per>95 && per<100){
+                textView8.setText("Your CGPA is : 10");
+                textView7.setText("Your percentage is : " + String.format("% .2f", per) + "%");
+            }
+            else if(per>100){
+                textView7.setText("Enter Valid Values!");
+                textView8.setText("");
+
+            }
+            else {
+                textView7.setText("Your percentage is : " + String.format("% .2f", per) + "%");
+                textView8.setText("Your CGPA is : " + String.format("% .1f", cgpa));
+            }
+        }catch (NumberFormatException e){
+            Toast.makeText(this, "Enter Valid Values!", Toast.LENGTH_SHORT).show();
 
         }
-        else {
-            textView7.setText("Your percentage is : " + String.format("% .2f", per) + "%");
-            textView8.setText("Your CGPA is : " + String.format("% .1f", cgpa));
-        }
+
+
+
     }
 }
--- app/src/main/java/com/ziro/calculate/MainActivity3.java
+++ app/src/main/java/com/ziro/calculate/MainActivity3.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
     EditText editTextNumberDecimal;
@@ -33,18 +34,19 @@ public class MainActivity3 extends AppCompatActivity {
     }
     @SuppressLint({"DefaultLocale", "SetTextI18n"})
     public void getCgpa(View view){
-        String s=editTextNumberDecimal.getText().toString();
-        double per=Double.parseDouble(s);
-        double cgpa=per/9.5;
-        if(per>100 || per==0){
-            textView5.setText("Enter Valid Value!");
+        try {
+            String s = editTextNumberDecimal.getText().toString();
+            double per = Double.parseDouble(s);
+            double cgpa = per / 9.5;
+            if (per > 100 || per == 0) {
+                textView5.setText("Enter Valid Value!");
+            } else if (per >= 95) {
+                textView5.setText("Your CGPA is : 10");
+            } else {
+                textView5.setText("Your CGPA is : " + String.format("% .1f", cgpa));
+            }
+        }catch (NumberFormatException e){
+            Toast.makeText(this, "Enter valid values!", Toast.LENGTH_SHORT).show();
         }
-        else if(per>=95){
-            textView5.setText("Your CGPA is : 10");
-        }
-        else {
-            textView5.setText("Your CGPA is : " + String.format("% .1f", cgpa));
-        }
-
     }
 }
